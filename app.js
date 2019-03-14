c10990364cf318c252d4f0b2b4ec4a1923929983
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2019-03-14 09:59:54 -0400

    Acomodando otro error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var schedule = require('node-schedule');
 const express =  require('express');
 const app = express();
 const bodyParser    = require('body-parser');
-const PORT =  3000;
+const PORT =  process.env.PORT || 3000;
 
 app.listen(PORT, () => {
      console.log(`Server is running on port ${PORT}`);
