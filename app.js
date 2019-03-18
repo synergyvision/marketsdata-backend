ddda925658678af4f82260b53d26eadf7f942f40
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2019-03-18 12:13:33 -0400

    Añadiendo dominio

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.listen(PORT, () => {
      console.log(`Server is running on port ${PORT}`);
 });
 
-// app.use(function(req, res, next) {
-//      res.header("Access-Control-Allow-Origin", "*");
-//      res.header("Access-Control-Allow-Methods", "POST, DELETE");
-//      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//      next();
-//    });
+app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+     res.header("Access-Control-Allow-Methods", "POST, DELETE");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+   });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
