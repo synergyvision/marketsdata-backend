79f5ff52f1772082cce2c8a016b5f803ed6e3a60
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2019-03-19 16:19:26 -0400

    Subiendo cambios

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const admin = require('firebase-admin');
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-    
+   
     admin.auth().createUser({
         email: req.body.email,
         password: req.body.password,
